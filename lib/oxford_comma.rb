062b6f298b985f5e9920cbfb7bd2da13a4ae3a65
Reilly Hartigan <reilly.hartigan@yale.edu>
2019-04-19 16:00:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,10 @@
 def oxford_comma(array)
   case array.length
-  when "1"
+  when 1
     string = array.join
-  when "2"
+  when 2
     string = array.join(" and ")
+  when > 3
   end
   array.join(", ")
 end
